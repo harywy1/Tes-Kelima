8eeaeee3e6c5af74c7ff15901fb376e61cbd1e0a
harywy1 <harywy1@gmail.com>
2019-01-08 15:29:17 +0700

    Tes Kelima add Cypress

---
+++ Instagram/cypress/integration/examples/IG.spec.js
@@ -0,0 +1,7 @@
+describe('Test Cypress soal 5', () => {
+    
+    it('Menuju ke situs', () => {
+        cy.visit('http://localhost:3000')
+    })
+
+})
---
+++ kontak-app/cypress/integration/examples/Kontak.spec.js
@@ -0,0 +1,7 @@
+describe('Test Cypress soal 4', () => {
+    
+    it('Menuju ke situs', () => {
+        cy.visit('http://localhost:3000')
+    })
+
+})
